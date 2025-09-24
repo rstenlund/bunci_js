d4da766b6fe1b1317d52e00554c1f99b62959ee9
uvota <ruben.stenlund@gmail.com>
2025-09-24 20:28:33 +0200

    Added bullets

---
+++ src/bullet.js
@@ -0,0 +1,48 @@
+export default class Bullet {
+  constructor(ctx, canvas_width, canvas_height, img_r, img_l) {
+    this.ctx = ctx;
+    this.width = 40;
+    this.height = 10;
+    this.canvas_height = canvas_height;
+    this.canvas_width = canvas_width;
+    this.x = 0;
+    this.y = 0;
+    this.dir = 1;
+    this.speed = 0.5;
+
+    this.img_r = img_r;
+    this.img_l = img_l;
+
+    this.reset();
+  }
+
+  reset() {
+    this.dir = Math.random() < 0.5 ? 1 : -1;
+    if (this.dir === 1) {
+      this.x = -1 * (Math.random() * 300 + 50);
+    } else {
+      this.x = this.canvas_width + Math.random() * 300 + 50;
+    }
+    this.y = Math.random() * (this.canvas_height - this.height);
+  }
+
+  draw() {
+    this.ctx.fillStyle = "red";
+    //this.ctx.fillRect(this.x + 5, this.y + 15, this.width - 12, this.height);
+    this.img = this.dir === 1 ? this.img_r : this.img_l;
+    this.ctx.drawImage(
+      this.img,
+      this.x,
+      this.y,
+      this.width,
+      (this.width / this.img.width) * this.img.height
+    );
+  }
+  update(deltaTime) {
+    this.x += this.speed * this.dir * deltaTime;
+
+    if (this.x < -500 || this.x > this.canvas_width + 500) {
+      this.reset();
+    }
+  }
+}
--- src/game.js
+++ src/game.js
@@ -1,6 +1,17 @@
 import bunciImage from "./assets/bunci.png";
 import playerImage from "./assets/player.png";
+import trophyImage from "./assets/trophy.png";
+import skott_r_Image from "./assets/skott_r.png";
+import skott_l_Image from "./assets/skott_l.png";
+
 import Player from "./player";
+import Bullet from "./bullet";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://zgabmcriwnxjjvcckkjm.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 function loadImage(src) {
   return new Promise((resolve, reject) => {
@@ -16,16 +27,57 @@ export default async function runGame(clerk_instance) {
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext("2d");
 
+  const { data, error } = await supabase.from("leaderboard").select("");
+  if (error) {
+    console.error("Error fetching leaderboard data:", error);
+    return;
+  }
+  let max_score = 0;
+
+  console.log(clerk_instance.user.username);
+  for (let entry of data) {
+    if (entry.user == clerk_instance.user.username) {
+      console.log("User found in leaderboard:", entry.user);
+      console.log("Score:", entry.score);
+      max_score = entry.score;
+    }
+  }
+  if (max_score === 0) {
+    const { data, error } = await supabase
+      .from("leaderboard")
+      .insert([{ user: clerk_instance.user.username, score: 0 }])
+      .select();
+    if (error) {
+      console.error("Error fetching leaderboard data:", error);
+      return;
+    }
+  }
+
+  console.log("Leaderboard data:", data);
+
   let running = false;
   let transition = false;
   let dead = false;
 
-  ctx.fillStyle = "lightgrey";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
   const img = await loadImage(bunciImage);
 
   const player_sprite = await loadImage(playerImage);
+
+  const skott_r_sprite = await loadImage(skott_r_Image);
+  const skott_l_sprite = await loadImage(skott_l_Image);
+
+  const trophy_sprite = await loadImage(trophyImage);
+  const trophySize = 60;
+
+  let bullets = [
+    new Bullet(
+      ctx,
+      canvas.width,
+      canvas.height,
+      skott_r_sprite,
+      skott_l_sprite
+    ),
+  ];
 
   let imageSizeFactor = 1;
   let a = 0;
@@ -44,6 +96,9 @@ export default async function runGame(clerk_instance) {
       if (y_off > canvas.height) {
         running = true;
         transition = false;
+        for (let bullet of bullets) {
+          bullet.reset();
+        }
       }
     }
     a += 0.05;
@@ -65,6 +120,14 @@ export default async function runGame(clerk_instance) {
       canvas.width / 2,
       canvas.height / 2 + 50 - y_off
     );
+
+    ctx.drawImage(
+      trophy_sprite,
+      canvas.width - trophySize - 20,
+      20 - y_off,
+      trophySize,
+      trophySize
+    );
   }
 
   function deathScreen() {
@@ -79,6 +142,11 @@ export default async function runGame(clerk_instance) {
         running = true;
         transition = false;
         dead = false;
+        score = 0;
+
+        for (let bullet of bullets) {
+          bullet.reset();
+        }
       }
     }
     a += 0.05;
@@ -100,6 +168,14 @@ export default async function runGame(clerk_instance) {
       canvas.width / 2,
       canvas.height / 2 + 50 - y_off
     );
+
+    ctx.drawImage(
+      trophy_sprite,
+      canvas.width - trophySize - 20,
+      20 - y_off,
+      trophySize,
+      trophySize
+    );
   }
 
   document.addEventListener("keypress", (e) => {
@@ -117,19 +193,56 @@ export default async function runGame(clerk_instance) {
   });
 
   canvas.addEventListener("mousedown", (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const canvasX = e.clientX - rect.left;
+    const canvasY = e.clientY - rect.top;
+    console.log("Canvas coordinates:", canvasX, canvasY);
+
+    if (!running && !transition) {
+      if (
+        canvasX > canvas.width - trophySize - 20 &&
+        canvasX < canvas.width - 20 &&
+        canvasY > 20 &&
+        canvasY < 20 + trophySize
+      ) {
+        // clicked on trophy
+        console.log("trophy clicked");
+        return;
+      }
+    }
     if (!running) {
       transition = true;
     }
-    if (e.button === 0 && running && e.clientX < canvas.width / 2) {
+    if (e.button === 0 && running && canvasX < canvas.width / 2) {
       player.left();
     }
-    if (e.button === 0 && running && e.clientX >= canvas.width / 2) {
+    if (e.button === 0 && running && canvasX >= canvas.width / 2) {
       player.right();
     }
   });
 
-  function gameLoop() {
-    ctx.fillStyle = "lightgrey";
+  let del = 0;
+
+  let l = Date.now();
+  let score = 0;
+
+  async function gameLoop() {
+    if (del < 1000) {
+      del++;
+    }
+
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const gradient = ctx.createRadialGradient(
+      canvas.width / 2,
+      canvas.height / 2,
+      0,
+      canvas.width / 2,
+      canvas.height / 2,
+      Math.max(canvas.width, canvas.height) / 1.2
+    );
+    gradient.addColorStop(0, "#cccccc"); // light grey center
+    gradient.addColorStop(1, "#444444"); // dark grey edges
+    ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     if (dead) {
@@ -143,6 +256,20 @@ export default async function runGame(clerk_instance) {
       return;
     }
 
+    if (Date.now() - l >= 1000) {
+      score++;
+      console.log(score);
+      l = Date.now();
+    }
+
+    //display score
+    ctx.font = "bold 24px Courier, monospace";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillText("Score: " + score, canvas.width / 2, 20);
+    ctx.fillText("Best: " + max_score, canvas.width / 2, 50);
+
     if (player.outOfBounds()) {
       dead = true;
       imageSizeFactor = 1;
@@ -151,9 +278,31 @@ export default async function runGame(clerk_instance) {
       y_off_speed = 0;
       transition = false;
       running = false;
+      if (score > max_score) {
+        max_score = score;
+        const { data, error } = await supabase
+          .from("leaderboard")
+          .update({ score: max_score })
+          .eq("user", clerk_instance.user.username)
+          .select();
+
+        if (error) {
+          console.error("Error updating leaderboard:", error);
+        } else {
+          console.log(data);
+        }
+      }
+      score = 0;
       deathScreen();
       requestAnimationFrame(gameLoop);
       return;
+    }
+
+    if (del > 300) {
+      for (let bullet of bullets) {
+        bullet.draw();
+        bullet.update(player.deltaTime);
+      }
     }
 
     player.draw();
