82d81a95fad9278328792fb0d94fe809dd0ef010
uvota <ruben.stenlund@gmail.com>
2025-09-25 23:04:37 +0200

    font size

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ if (clerk.isSignedIn) {
   clerk.mountUserButton(userButtonDiv);
 } else {
   document.getElementById("app").innerHTML = `
-  <h1 style="width: 20vw; margin-bottom: 20px; text-align: center; font-family: 'Courier New', monospace;">bunci</h1>
+  <h1 style="width: 20vw; margin-bottom: 20px; text-align: center; font-family: 'Courier New', monospace; font-size: 40px;">bunci</h1>
     <div id="sign-in"></div>
 
   `;
