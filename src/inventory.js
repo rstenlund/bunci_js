849a6ed08f07b6c6bcd10c07daec407c0d98757c
uvota <ruben.stenlund@gmail.com>
2025-10-05 16:45:18 +0200

    adjust inventory count text positioning

--- src/inventory.js
+++ src/inventory.js
@@ -48,7 +48,7 @@ export default class Inventory {
       this.ctx.fillText(
         this.count,
         this.x + this.size - 15,
-        this.y + this.size - 20
+        this.y + this.size - 22
       );
     }
   }
